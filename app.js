088565058885f6c39d262ab4330159425fc6638e
Olahben <ohagenbendixen@gmail.com>
2023-06-26 10:55:33 +0100

    Create LinkedList and node factory

---
+++ app.js
@@ -0,0 +1,15 @@
+function linkedList() {}
+
+function node(data = null, next = null) {}
+
+/*
+Linkedlist:
+Represents the full list
+
+Contains functions that operates on the linkedList
+
+node:
+Represents a node in the list
+has two properties, data, and next. By default they are null
+
+*/
