ca1b9df0c3ac2643c00cb317db877a8f759ba4fe
Olahben <ohagenbendixen@gmail.com>
2023-06-27 10:26:54 +0100

    Refactor appendNode func to add a new node to the beggining... of the list, instead of the end.

--- app.js
+++ app.js
@@ -8,17 +8,6 @@ function linkedList() {
     next: null,
   };
 
-  function appendNode(value) {
-    const newNode = node(value);
-    head.next = newNode;
-  }
-
-  function prependNode(value) {
-    // console.log(`Head.next: ${head.next}`);
-    const newNode = node(value, head.next);
-    head.next = newNode;
-  }
-
   function size() {
     // console.log(head.next);
     let nodes = 0;
@@ -35,17 +24,34 @@ function linkedList() {
     return nodes;
   }
 
-  function headNode() {
-    // console.log(head.next.data);
-    return head.next;
-  }
-
   function tailNode() {
     function getLastNode(obj) {
+      // console.log(obj);
       if (obj.next === null) return obj;
       return getLastNode(obj.next);
     }
     return getLastNode(head.next);
+  }
+
+  function prependNode(value) {
+    // console.log(`Head.next: ${head.next}`);
+    const newNode = node(value, head.next);
+    head.next = newNode;
+  }
+
+  function appendNode(value) {
+    if (size() === 0) {
+      prependNode(value);
+    } else {
+      const newNode = node(value);
+      const lastNode = tailNode();
+      lastNode.next = newNode;
+    }
+  }
+
+  function headNode() {
+    // console.log(head.next.data);
+    return head.next;
   }
 
   return {
@@ -63,6 +69,8 @@ list.appendNode("data");
 list.prependNode("data2");
 list.prependNode("data3");
 list.prependNode("data4");
+list.appendNode("data1");
 console.log(list.size());
 console.log(list.headNode());
 console.log(list.tailNode());
+console.log(list.head.next);
